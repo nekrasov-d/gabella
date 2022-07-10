bdb1fd4b2f34cf3e64d7a6a9c47c48d19591f900
Dmitry Nekrasov <bluebag@yandex.ru>
2022-07-10 21:24:27 +0300

    Add .gitignore, fix low pass filter

--- rtl/low_pass_filter.sv
+++ rtl/low_pass_filter.sv
@@ -14,8 +14,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- *
- * XXX: add annotation
+ * A dump low-pas filter with coarse band selection (power of two-s).
+ * The trick is that a basic low-pass requires equal coefficients.
+ * This way we can avoid waisting multipliers. Just sum all the delayed data and
+ * shift right (that costs nothing).
 */
 
 module low_pass_filter #(
@@ -30,15 +32,21 @@ module low_pass_filter #(
   output logic [DWIDTH-1:0] data_o
 );
 
-logic [DWIDTH-1:0]               data_d [DEPTH-1:0];
+logic [DWIDTH-1+$clog2(DEPTH):0] data_d [DEPTH-1:0];
 logic [DWIDTH-1+$clog2(DEPTH):0] sum    [DEPTH:0];
-
+logic [DWIDTH-1+$clog2(DEPTH):0] data_i_sign_extended;
 logic [DWIDTH-1:0] data_abs;
+
+always_comb
+  if( data_i[DWIDTH-1] )
+    data_i_sign_extended = { {$clog2(DEPTH){1'b1}}, data_i };
+  else 
+    data_i_sign_extended = { {$clog2(DEPTH){1'b0}}, data_i };
 
 always_ff @( posedge clk_i )
   if( sample_tick_i )
     begin
-      data_d[0] <= data_i;//{~data_i[DWIDTH-1], data_i[DWIDTH-2:0]};
+      data_d[0] <= data_i_sign_extended;
       for( int i = 0; i < DEPTH-1; i++ )
         data_d[i+1] <= data_d[i];
     end
@@ -53,6 +61,6 @@ always_comb
 always_ff @( posedge clk_i )
   data_abs <= sum[DEPTH] >> $clog2(DEPTH);
 
-assign data_o = enable_i ? data_abs : data_i;// {~data_abs[DWIDTH], data_abs[DWIDTH-1:0]} : data_i;
+assign data_o = enable_i ? data_abs : data_i;
 
 endmodule
