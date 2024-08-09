abc28ec2586570eaf96bc3bd10ddb2bda9f21b1f
Dmitry Nekrasov <bluebag@yandex.ru>
2024-08-09 21:35:49 +0300

    the 'design_under_test' module placeholder is now annotated

--- rtl/other_effects/design_under_test.sv
+++ rtl/other_effects/design_under_test.sv
@@ -1,4 +1,32 @@
-
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Dmitriy Nekrasov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * ---------------------------------------------------------------------------------
+ *
+ * A blank placeholder to test new raw stuff
+ *
+ * -- Dmitry Nekrasov <bluebag@yandex.ru>   Mon, 08 Apr 2024 08:50:31 +0300
+ */
 
 
 module design_under_test #(
@@ -11,5 +39,9 @@ module design_under_test #(
   input        [DWIDTH-1:0] data_i,
   output logic [DWIDTH-1:0] data_o
 );
+
+// Your test design here
+
+assign data_o = data_i;
 
 endmodule
